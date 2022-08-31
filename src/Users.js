ec1b608601b1a0ce4026590dd5256c57f1b7d7cc
losajjang <losajjang@naver.com>
2022-09-01 01:11:53 +0900

    API 연동의 기본

---
+++ src/Users.js
@@ -0,0 +1,48 @@
+import axios from 'axios';
+import React, {useEffect, useState} from 'react';
+
+function Users() {
+  const [users, setUsers] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchUser = async () => {
+    try {
+      // 요청이 시작 할 때에는 error와 users를 초기화하고
+      setError(null);
+      setUsers(null);
+      // loading 상태를 true 로 바꿉니다.
+      setLoading(true);
+      const response = await axios.get(
+        'https://jsonplaceholder.typicode.com/users',
+      );
+      setUsers(response.data); // 데이터는 response.data 안에 들어있습니다.
+    } catch (e) {
+      setError(e);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  if (loading) return <div>로딩중...</div>;
+  if (error) return <div>에러가 발생했습니다.</div>;
+  if (!users) return null;
+
+  return (
+    <>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>
+            {user.username} ({user.name})
+          </li>
+        ))}
+      </ul>
+      <button onClick={fetchUser}>다시 불러오기</button>
+    </>
+  );
+}
+
+export default Users;
